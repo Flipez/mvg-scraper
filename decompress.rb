1579fa45be4e0081e62e3b3b1be0dada1ce93687
Flipez <code@brauser.io>
2024-02-17 17:52:08 +0100

    feat(decompressor): add streaming example
    Signed-off-by: Flipez <code@brauser.io>

--- decompress.rb
+++ decompress.rb
@@ -1,11 +1,20 @@
 require 'zstds'
 require 'minitar'
+require 'json'
+
+stations = {}
 
 ZSTDS::Stream::Reader.open "20240217.tar.zst" do |reader|
   Minitar::Reader.open reader do |tar|
     tar.each_entry do |entry|
-      puts entry.name
-      entry.read
+      if entry.name.end_with? "meta.json"
+        content = JSON.parse(entry.read)
+        stations[content['request_params']['globalId']] = stations[content['request_params']['globalId']].to_i + 1
+      end
     end
   end
+end
+
+stations.sort_by{|k,v| k }.each do |k,v|
+  puts "#{k}\t: #{v}"
 end
