c20a0c9d14d67d6b44a16e454808761904e35103
Flipez <code@brauser.io>
2024-02-18 20:59:12 +0100

    fix(compress): sort files by name
    Signed-off-by: Flipez <code@brauser.io>

--- misc/compress.sh
+++ misc/compress.sh
@@ -2,5 +2,5 @@
 
 yesterday=$(date -d '-1 day' "+%Y%m%d")
 
-tar --zstd -cf $yesterday.tar.zst $yesterday/
+tar --sort=name --zstd -cf $yesterday.tar.zst $yesterday/
 rsync -av $yesterday.tar.zst /mnt/storagebox/mvg-data/
