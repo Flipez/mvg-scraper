4357513b6775c4b672b53b8b075e5e613db59ce5
Robert Müller <code@brauser.io>
2024-02-24 14:58:36 +0100

    feat(compressor): improve compress script
    Signed-off-by: Robert Müller <code@brauser.io>

--- misc/compress.sh
+++ misc/compress.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+set -o errexit
 
+beforeyesterday=$(date -d '-2 day' "+%Y%m%d")
 yesterday=$(date -d '-1 day' "+%Y%m%d")
 
+###
+# Created tar from the requests yesterday using zstandard compression
 tar --sort=name --zstd -cf $yesterday.tar.zst $yesterday/
+
+###
+# Test tar integrity by listing its content
+tar -tf $yesterday.tar.zst > /dev/null
+
+###
+# Copy compressed file into storagebox for public access
 rsync -av $yesterday.tar.zst /mnt/storagebox/mvg-data/
+
+###
+# Remove the day before yesterday
+rm -r $beforeyesterday/
+
+###
+# Perform healthcheckping if all previous commands succeed
+curl "https://hc-ping.com/8dbf4a8e-0629-4876-899c-6d702c754bc1"
