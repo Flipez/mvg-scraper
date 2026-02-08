f29aad488e2d1fdf8f8fe4dffa23adcd2121a03e
Flipez <gh@flipez.net>
2026-02-08 13:30:46 +0100

    add docker image, improve ruby stuff
    Signed-off-by: Flipez <gh@flipez.net>

--- lib/mvg.rb
+++ lib/mvg.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-require_relative 'mvg/scraper'
 require_relative 'mvg/version'
+require_relative 'mvg/scraper'
+require_relative 'mvg/station_updater'
+require_relative 'mvg/cli'
 
 ###
 # Provides the MVG Scraper functionality
